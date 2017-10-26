a119d023255c54e18ff4f222c5f225ef5c32cefb
monta <monta@Desktop>
2017-10-25 23:38:05 -0300

    pila

--- src/appxml/Verificador.java
+++ src/appxml/Verificador.java
@@ -36,40 +36,9 @@ public class Verificador {
                  }
                  
             }
-//            if (p.push(datos[i][i])) {
-//                System.out.println("push linea " + i + datos[i]);
-//            } else {
-//                System.out.println(i + " dato no ingresado");
-//            }
+
         }
-//        //recorrer vector de string
-//        for (int i = 0; i < 1; i++) {
-//            valor = p.pop();
-//            //recorremos por caracter buscando el cierre de '<'
-//            for(int j = 0 ; j<valor.length();j++){
-//                if (valor.charAt(j)=='<'){
-//                    // si se encontro '<' pero es el último caracter, faltará '>'
-//                    if(j==valor.length()-1){
-//                        System.out.println("Secuencia incorrecta termino línea "+i+" se esperaba '>'");
-//                        break;
-//                    }
-//                    //recorremos por caracter buscando el cierre de '>'
-//                    for(int k=j+1;k<valor.length();k++){
-//                        if (valor.charAt(k)=='>'){  
-//                            System.out.println("Secuencia correcta linea " +i);
-//                            j=k;
-//                            break;                    
-//                        } else {
-//                            System.out.println("Secuencia incorrecta termino línea "+i);
-//                            j=k;
-//                        }               
-//                    }
-//                } else {
-//                    System.out.println("Secuencia incorrecta línea "+i+" se esperaba un '<'");
-//                }
-//            } 
-//           
-//        }
+
         return false;
 
     }
